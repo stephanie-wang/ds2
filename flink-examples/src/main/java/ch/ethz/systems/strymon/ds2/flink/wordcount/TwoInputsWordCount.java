d602d92ea365a085dd8fac0dce7fc62d1cfdf538
John Liagouris <liagos@inf.ethz.ch>
2019-04-18 13:30:36 -0700

    Fix source rate control

--- flink-examples/src/main/java/ch/ethz/systems/strymon/ds2/flink/wordcount/TwoInputsWordCount.java
+++ flink-examples/src/main/java/ch/ethz/systems/strymon/ds2/flink/wordcount/TwoInputsWordCount.java
@@ -42,7 +42,8 @@ public class TwoInputsWordCount {
 		final DataStream<String> textTwo = env.addSource(
 				new RateControlledSourceFunction(
 						params.getInt("source-rate", 80000),
-						params.getInt("sentence-size", 100)))
+						params.getInt("sentence-size", 100),
+						params.getInt("max-sentences", 100000)))
 				.name("Source Two")
 				.setParallelism(params.getInt("p1", 1));
 
