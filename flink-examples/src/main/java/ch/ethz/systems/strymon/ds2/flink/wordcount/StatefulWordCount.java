72f2bc5293acbf0099a93d39ab2d182ae1adc140
John Liagouris <liagos@inf.ethz.ch>
2019-04-19 00:13:49 -0700

    Change latency logging

--- flink-examples/src/main/java/ch/ethz/systems/strymon/ds2/flink/wordcount/StatefulWordCount.java
+++ flink-examples/src/main/java/ch/ethz/systems/strymon/ds2/flink/wordcount/StatefulWordCount.java
@@ -78,7 +78,7 @@ public class StatefulWordCount {
 						final String filename = "/flink-1.7.2/latencies-" + n + ".log";
 						FileOutputStream is = new FileOutputStream(filename);
 						OutputStreamWriter osw = new OutputStreamWriter(is);
-						BufferedWriter w = new BufferedWriter(osw);
+						BufferedWriter fileWriter = new BufferedWriter(osw);
 
 						public void invoke(Tuple3<Long, String, Long> value) throws IOException {
 							fileWriter.write(""+value.f0);
